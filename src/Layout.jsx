9fa9458cde51a0c4a26bd440fa34aad796003521
aasuyadav1 <aasuy284@gmail.com>
2024-05-11 13:51:51 +0530

    update mobile view

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,32 +3,36 @@ import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Aside from './components/Aside'
 import { Link } from 'react-router-dom'
+import useStore from './lib/store'
 
 const Layout = () => {
+  const {initialState} = useStore((state) => state)
 
   return (
     <>
-      <div className="p-relative bg-[#0c1218]">
-        <div className="flex justify-center">
+      <div className="p-relative bg-[#0c1218] w-full">
+        <div className="flex justify-center w-full">
           <Navbar />
-          <main>
-            <div className="flex md:w-[990px] w-full">
-              <section className="border border-y-0 border-gray-800 max-w-[600px] w-full">
-                 <div className="flex">
-                  <div className="flex-1 mx-2">
-                    <h2 className="px-4 py-2 text-xl font-semibold text-white">Home</h2>
+          <main className="max-w-screen-lg w-full">
+            <div className="flex flex-col md:flex-row md:w-full">
+              <section className="border border-y-0 border-gray-800 md:max-w-[600px] w-full">
+                <div className="flex justify-between items-center">
+                  <div className="mx-2">
+                   <Link to="/">
+                   <h2 className="px-4 py-2 text-xl font-semibold text-white">Home</h2>
+                   </Link>
                   </div>
-                  <div className="flex-1 px-4 py-2 mx-2">
-                    <Link
-                      
-                      className="text-2xl font-medium rounded-full text-white hover:bg-gray-800 hover:text-blue-300 float-right"
-                    >
-                      <svg className="m-2 h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                        <g>
-                          <path d="M22.772 10.506l-5.618-2.192-2.16-6.5c-.102-.307-.39-.514-.712-.514s-.61.207-.712.513l-2.16 6.5-5.62 2.192c-.287.112-.477.39-.477.7s.19.585.478.698l5.62 2.192 2.16 6.5c.102.306.39.513.712.513s.61-.207.712-.513l2.16-6.5 5.62-2.192c.287-.112.477-.39.477-.7s-.19-.585-.478-.697zm-6.49 2.32c-.208.08-.37.25-.44.46l-1.56 4.695-1.56-4.693c-.07-.21-.23-.38-.438-.462l-4.155-1.62 4.154-1.622c.208-.08.37-.25.44-.462l1.56-4.693 1.56 4.694c.07.212.23.382.438.463l4.155 1.62-4.155 1.622zM6.663 3.812h-1.88V2.05c0-.414-.337-.75-.75-.75s-.75.336-.75.75v1.762H1.5c-.414 0-.75.336-.75.75s.336.75.75.75h1.782v1.762c0 .414.336.75.75.75s.75-.336.75-.75V5.312h1.88c.415 0 .75-.336.75-.75s-.335-.75-.75-.75zm2.535 15.622h-1.1v-1.016c0-.414-.335-.75-.75-.75s-.75.336-.75.75v1.016H5.57c-.414 0-.75.336-.75.75s.336.75.75.75H6.6v1.016c0 .414.335.75.75.75s.75-.336.75-.75v-1.016h1.098c.414 0 .75-.336.75-.75s-.336-.75-.75-.75z"></path>
-                        </g>
-                      </svg>
-                    </Link>
+                  <div className="mx-2">
+                  {
+                initialState?.userData && initialState?.userData?._id ? (
+                    <Link to={`/profile/${initialState.userData._id}`}  class="group flex items-center px-2 py-2 text-base leading-6 font-semibold rounded-full hover:text-blue-300">
+                <svg class="mr-4 h-6 w-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
+                    <path d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
+                </svg>
+          
+            </Link>
+                ) : null
+            }
                   </div>
                 </div>
                 <Outlet />
--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -3,67 +3,65 @@ import { Link } from 'react-router-dom'
 
 const Aside = () => {
   return (
-    <aside className="w-2/5 h-12 position-relative md:block hidden">
-    {/*Aside menu (right side)*/}
-    <div style={{ maxWidth: 350 }}>
-      <div className="overflow-y-auto fixed  h-screen">
-        <div className="relative text-gray-300 w-80 p-5">
-          <button type="submit" className="absolute ml-4 mt-3 mr-4">
-            <svg
-              className="h-4 w-4 fill-current"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              version="1.1"
-              id="Capa_1"
-              x="0px"
-              y="0px"
-              viewBox="0 0 56.966 56.966"
-              style={{ enableBackground: "new 0 0 56.966 56.966" }}
-              xmlSpace="preserve"
-              width="512px"
-              height="512px"
-            >
-              <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
-            </svg>
-          </button>
-          <input
-            type="search"
-            name="search"
-            placeholder="Search Twitter"
-            className=" bg-dim-700 h-10 px-10 pr-5 w-full rounded-full text-sm focus:outline-none bg-purple-white shadow rounded border-0"
-          />
-        </div>
-        {/*trending tweet section*/}
-        <div className="max-w-sm rounded-lg bg-dim-700 overflow-hidden shadow-lg m-4">
-          <div className="flex">
-            <div className="flex-1 m-2">
-              <h2 className="px-4 py-2 text-xl w-48 font-semibold text-white">
-                Germany trends
-              </h2>
-            </div>
-            <div className="flex-1 px-4 py-2 m-2">
-              <Link
-                href=""
-                className=" text-2xl rounded-full text-white hover:bg-gray-800 hover:text-blue-300 float-right"
+    <aside className="md:w-2/5 h-12 md:relative fixed right-0 md:block hidden">
+      {/* Aside menu (right side) */}
+      <div style={{ maxWidth: 350 }}>
+        <div className="overflow-y-auto h-screen">
+          <div className="relative text-gray-300 w-full p-5">
+            <button type="submit" className="absolute ml-4 mt-3 md:mr-4">
+              <svg
+                className="h-4 w-4 fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                version="1.1"
+                id="Capa_1"
+                x="0px"
+                y="0px"
+                viewBox="0 0 56.966 56.966"
+                style={{ enableBackground: "new 0 0 56.966 56.966" }}
+                xmlSpace="preserve"
+                width="512px"
+                height="512px"
               >
-                <svg
-                  className="m-2 h-6 w-6"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path>
-                  <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                </svg>
-              </Link>
-            </div>
+                <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+              </svg>
+            </button>
+            <input
+              type="search"
+              name="search"
+              placeholder="Search Twitter"
+              className="bg-dim-700 h-10 px-10 pr-5 w-full rounded-full text-sm focus:outline-none bg-purple-white shadow rounded border-0"
+            />
           </div>
-          <hr className="border-gray-800" />
-          {/*first trending tweet*/}
-          <div className="flex">
+          {/* Trending tweet section */}
+          <div className="max-w-sm rounded-lg bg-dim-700 overflow-hidden shadow-lg m-4">
+            <div className="flex">
+              <div className="flex-1 m-2">
+                <h2 className="px-4 py-2 text-xl w-48 font-semibold text-white">
+                  Germany trends
+                </h2>
+              </div>
+              <div className="flex-1 px-4 py-2 m-2">
+                <Link
+                  href=""
+                  className="text-2xl rounded-full text-white hover:bg-gray-800 hover:text-blue-300 float-right"
+                >
+                  <svg
+                    className="m-2 h-6 w-6"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path>
+                    <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                  </svg>
+                </Link>
+              </div>
+            </div>
+            <div className="flex">
             <div className="flex-1">
               <p className="px-4 ml-2 mt-3 w-48 text-xs text-gray-400">
                 1 . Trending
